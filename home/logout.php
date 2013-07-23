d7b7dde933bc431f4aa8ec47acb72283eb37a01f
ngoalongkt <ngoalongkt@ngoalongkt-PC>
2013-07-23 09:05:05 +0700

    db

--- home/logout.php
+++ home/logout.php
@@ -8,5 +8,5 @@
 	// delete the cookie
 	tep_setcookie("account_number",$account_number	, time() -1, HTTP_COOKIE_PATH, HTTP_COOKIE_DOMAIN);
 	tep_setcookie("password",$login_password, time() -1, HTTP_COOKIE_PATH, HTTP_COOKIE_DOMAIN);			 
-	tep_redirect(get_href_link(PAGE_DEFAULT));	
+	tep_redirect(get_href_link(PAGE_DEFAULT,'','SSL'));	
 ?>
