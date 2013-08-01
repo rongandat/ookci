975189e6336e6fee88316c415a25c7f0d127c2df
ngoalongkt <ngoalongkt@ngoalongkt-PC>
2013-08-01 09:04:08 +0700

    db

--- includes/error_code.php
+++ includes/error_code.php
@@ -10,5 +10,12 @@ $__ERROR_CODE['ERR_007'] = 'Currency must correspond to PAYEE_ACCOUNT';
 $__ERROR_CODE['ERR_008'] = 'Currency must correspond to PAYER_ACCOUNT';
 $__ERROR_CODE['ERR_009'] = 'Amount must be numberic and greater than zero';
 $__ERROR_CODE['ERR_010'] = 'Currency does not exists in our system';
+$__ERROR_CODE['ERR_011'] = 'Acount does not exists in our system';
+$__ERROR_CODE['ERR_012'] = 'Acount to does not exists in our system or you haven\'t yet enable API';
+$__ERROR_CODE['ERR_013'] = 'Amount is not correct format';
+$__ERROR_CODE['ERR_014'] = 'Curency is not correct format';
+$__ERROR_CODE['ERR_015'] = 'Transaction is exists';
+$__ERROR_CODE['ERR_016'] = 'Your amount is not enough to perform this transaction';
+$__ERROR_CODE['ERR_017'] = 'You must accept to post method';
 
 ?>
--- ookpanel/home/index.php
+++ ookpanel/home/index.php
@@ -1,5 +1,18 @@
 <?php
 
 include('includes/admin_login_check.php');
+
+for ($m = 1; $m <= 12; $m++) {
+    $sqlUserMonth = 'select * from ' . _TABLE_USERS .' where MONTH(signup_date) = '.$m.' AND YEAR(signup_date) = '.date("Y");
+    $check_email_query = db_query($sqlUserMonth);
+    $countUsers[$m] = db_num_rows($check_email_query);
+    
+//    $sqlUserMonth = 'select SUM() from ' . _TABLE_USERS .' where MONTH(signup_date) = '.$m.' AND YEAR(signup_date) = '.date("Y");
+//    $check_email_query = db_query($sql_check_email);
+//    $countUsers[$m] = db_num_rows($check_email_query);
+}
+
+$smarty->assign('count_user',$countUsers);
+
 $_html_main_content = $smarty->fetch('home/home.html');
 ?>
